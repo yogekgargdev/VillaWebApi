6f20e9b10dc6d0898d7f0810ce5ec35d5a5e7ed0
Yogek.Garg <Yogek.Garg@DESKTOP-56R8DEK>
2023-08-02 17:44:00 +0530

    Post Controller Addition

--- VillaWebApi_Project/Controllers/VillaWebApiController.cs
+++ VillaWebApi_Project/Controllers/VillaWebApiController.cs
@@ -11,17 +11,51 @@ namespace VillaWebApi_Project.Controllers
 
     {
         [HttpGet]
-        public IEnumerable<VillaDto> GetVillas()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<VillaDto>> GetVillas()
         {
-            return VillaStore.VillaData;
+            return Ok(VillaStore.VillaData);
         }
 
         //[HttpGet("Id")] To explicity tell we want a integer we use the below syntax, also one thing to 
         // note is we had to add the route in [HttpGet] or else it was showing ambigious functions to call
-        [HttpGet("{Id:int}")]
-        public VillaDto GetVilla(int Id)
+        [HttpGet("{Id:int}", Name = "AddVilla")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<VillaDto> GetVilla(int Id)
         {
-            return VillaStore.VillaData.FirstOrDefault(result => result.Id == Id);
+            if (Id == 0)
+            {
+                return BadRequest();
+            }
+            var DataToRet = VillaStore.VillaData.FirstOrDefault(result => result.Id == Id);
+            if (DataToRet == null)
+            {
+                return NotFound();
+            }
+            return Ok(DataToRet);
+
+        }
+
+        [HttpPost]
+        public ActionResult<VillaDto> AddVilla([FromBody] VillaDto villaDto)
+        {
+            if (villaDto == null)
+            {
+                return BadRequest();
+            }
+            if (villaDto.Id > 0)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            int NewId = VillaStore.VillaData.OrderByDescending(result => result.Id).FirstOrDefault().Id + 1;
+            VillaDto VillaDtoToAdd = new VillaDto { Id = NewId, Name = villaDto.Name };
+
+            VillaStore.VillaData.Add(VillaDtoToAdd);
+            //return Ok(VillaDtoToAdd);
+            return CreatedAtRoute("AddVilla", new { Id = NewId }, VillaDtoToAdd);
         }
 
     }
